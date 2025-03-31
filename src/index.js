84e6bbcfd8482f581e8583063fa119063c1fbaea
Bikash2448 <bikashcse12448@gmail.com>
2025-03-31 14:49:28 +0530

    Final update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ dotenv.config({path:"./.env"})
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 app.use(route)
 
 async function connectDb(){
